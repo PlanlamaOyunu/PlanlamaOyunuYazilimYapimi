8600fc4903085aa61567d83f545a6446bd109905
Emreturkm <emre.turkmen.123@hotmail.com>
2021-05-11 18:02:44 +0300

    Add project files.

---
+++ Forms/GirisKayitFrm.cs
@@ -0,0 +1,122 @@
+﻿using PlanlamaOyunuYazilimYapimi.Forms;
+using PlanlamaOyunuYazilimYapimi.SqlQuerys;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanlamaOyunuYazilimYapimi
+{
+    public partial class GirisKayitFrm : Form
+    {
+        public GirisKayitFrm()
+        {
+            InitializeComponent();
+        }
+
+        GirisKayitFrmQuerys grsKytFrmQrys = new GirisKayitFrmQuerys();
+        /*
+         * GİRİŞ KISMI
+         */
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            if (txtBoxGirisKullaniciAdi.Text.Trim() != "" && txtBoxGirisSifre.Text.Trim() != "")
+            {
+                try
+                {
+                    string yetki;
+                    yetki = grsKytFrmQrys.girisKontrol(txtBoxGirisKullaniciAdi.Text.Trim(), txtBoxGirisSifre.Text.Trim());//giris kontrole gönderilen kullanıcı adı ve şifresine sahip kullanıcı var mı diye veritabanında sorgu yaptırıyoruz
+                    if (yetki != null)//sorgudan bir sonuç çıkarsa if e giriyor
+                    {
+                        if (yetki == "ALICI")
+                        {
+                            //alıcı formuna yönlendirilecek
+                            this.Hide();
+                            AliciMenuFrm aliciMenuFrm = new AliciMenuFrm();
+                            aliciMenuFrm.StartPosition = FormStartPosition.CenterScreen;
+                            aliciMenuFrm.alici.kullaniciAdi = txtBoxGirisKullaniciAdi.Text;
+                            aliciMenuFrm.Closed += (s, args) => this.Close();
+                            aliciMenuFrm.Show();
+                        }
+                        else if (yetki == "SATICI")
+                        {
+                            //satıcı formuna yönlendirilecek
+                            this.Hide();
+                            SaticiMenuFrm saticiMenuFrm = new SaticiMenuFrm();
+                            saticiMenuFrm.StartPosition = FormStartPosition.CenterScreen;
+                            saticiMenuFrm.Closed += (s, args) => this.Close();
+                            saticiMenuFrm.Show();
+                        }
+                        else if (yetki == "ADMİN")
+                        {
+                            //admin formuna yönlendirilecek
+                            this.Hide();
+                            AdminMenuFrm adminMenuFrm = new AdminMenuFrm();
+                            adminMenuFrm.StartPosition = FormStartPosition.CenterScreen;
+                            adminMenuFrm.Closed += (s, args) => this.Close();
+                            adminMenuFrm.Show();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Böyle bir kullanıcı mevcut değil veya şifreniz yanlış!", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.ToString());
+                    throw;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen boş kutucuk bırakmayınız.", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        /*
+         * KAYIT KISMI
+         */
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            if (txtBoxKayitAd.Text.Trim() == "" || txtBoxKayitSoyad.Text.Trim() == "" || txtBoxKayitKullaniciAdi.Text.Trim() == "" ||
+                txtBoxKayitSifre.Text.Trim() == "" || txtBoxKayitTcNo.Text.Trim() == ""
+                 || txtBoxKayitTelefonNo.Text.Trim() == "" || txtBoxKayitEmail.Text.Trim() == "" || txtBoxKayitAdres.Text.Trim() == "")
+            {
+                MessageBox.Show("Lütfen boş kutucuk bırakmayınız!");
+            }
+            else
+            {
+                kayitOl();
+            }
+        }
+        public void kayitOl()
+        {
+
+            if (grsKytFrmQrys.kayitKontrol(txtBoxKayitKullaniciAdi.Text.Trim(), txtBoxKayitTcNo.Text.Trim(), txtBoxKayitTelefonNo.Text.Trim(), txtBoxKayitEmail.Text.Trim()))//parametre olarak gönderilenler başka kullanıcıda aynı mı diye kontrol ediyor
+            {
+                string yetki = "";
+                if (rBtnAlici.Checked)//alıcı olarak kayıt yapacaksak yetkiyi alıcı diye kaydediyor
+                {
+                    yetki = "ALICI";
+
+                }
+                else if (rBtnSatici.Checked)//satıcı olarak kayıt yapacaksak yetkiyi satıcı diye kaydediyor
+                {
+                    yetki = "SATICI";
+                }
+                //kayıt işlemi yaptırılıyor
+                grsKytFrmQrys.kayitOl(txtBoxKayitAd.Text.Trim(), txtBoxKayitSoyad.Text.Trim(), txtBoxKayitKullaniciAdi.Text.Trim(), txtBoxKayitSifre.Text.Trim(),
+                        txtBoxKayitTcNo.Text.Trim(), txtBoxKayitTelefonNo.Text.Trim(), txtBoxKayitEmail.Text.Trim(), txtBoxKayitAdres.Text.Trim(), yetki);
+            }
+            else
+            {
+                MessageBox.Show("Aynı Kullanıcı adı, TC No, Telefon No veya Email'e sahip kullanıcı mevcuttur.");
+            }
+        }
+    }
+}
---
+++ SqlQuerys/GirisKayitFrmQuerys.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanlamaOyunuYazilimYapimi.SqlQuerys
+{
+    public class GirisKayitFrmQuerys
+    {
+        public static string connectionSource = Properties.Settings.Default.SqlString;//properties bölümünden veritabanı bağlantı stringini alıyor
+        SqlConnection baglanti = new SqlConnection(connectionSource);//sql bağlantısını tanımlıyoruz
+        /*
+         * GİRİŞ SORGULARI
+         */
+        public string girisKontrol(string kullaniciAdi, string sifre)
+        {
+            string yetki = null;
+            try
+            {
+                baglanti.Open();//veritabanı ile olan bağlantıyı açıyor
+                SqlCommand komut = new SqlCommand("Select * from tblKullanici where kullaniciAdi = @KullaniciAdi and sifre = @Sifre", baglanti);//sorgumuz
+                komut.Parameters.AddWithValue("@KullaniciAdi", kullaniciAdi);//parametreler
+                komut.Parameters.AddWithValue("@Sifre", sifre);
+                SqlDataReader read = komut.ExecuteReader();//sorgudan dönen değerleri okuyor
+                while (read.Read())//eğer gönderdiğimiz kullanıcı adı ve sifresine sahip birisi var ise yetki değişkenini dolduruyor. yoksa yetki null kalıyor
+                {
+                    Properties.Settings.Default.kullaniciID = Convert.ToInt32(read["kullaniciID"]);//kullanıcıID yi diğer ekranlarda kullanmak için properties kısmına kaydettim
+                    yetki = read["yetki"].ToString();
+                }
+                baglanti.Close();//veritabanı ile olan bağlantıyı kapatıyor
+                return yetki;//yetki değişkenini ilk çağırdığımız yere döndürüyor
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.ToString());
+                throw;
+            }
+        }
+        /*
+         * KAYIT SORGULARI
+         */
+        public bool kayitKontrol(string kullaniciAdi, string tcNo, string telefonNo, string email)
+        {
+            bool izin = true;
+            try
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("Select * from tblKullanici where kullaniciAdi = @KullaniciAdi or tcNo = @tcNo or telefonNo = @telefonNo" +
+                    " or email = @email", baglanti);
+                komut.Parameters.AddWithValue("@KullaniciAdi", kullaniciAdi);
+                komut.Parameters.AddWithValue("@tcNo", tcNo);
+                komut.Parameters.AddWithValue("@telefonNo", telefonNo);
+                komut.Parameters.AddWithValue("@email", email);
+                SqlDataReader read = komut.ExecuteReader();
+                while (read.Read())
+                {
+                    izin = false;
+                }
+                baglanti.Close();
+                return izin;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.ToString());
+                throw;
+            }
+        }
+
+        public void kayitOl(string ad, string soyad, string kullaniciAdi, string sifre, string tcNo, string telefonNo, string email, string adres, string yetki)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("INSERT INTO tblKullanici (ad,soyad,kullaniciAdi,sifre,tcNo,telefonNo,email,adres,yetki,bakiye) VALUES " +
+                    "(@ad,@soyad,@kullaniciAdi,@sifre,@tcNo,@telefonNo,@email,@adres,@yetki,0)", baglanti);
+                //sql sorgusuna parametre degerlerini yolluyoruz
+                komut.Parameters.AddWithValue("@ad", ad);
+                komut.Parameters.AddWithValue("@soyad", soyad);
+                komut.Parameters.AddWithValue("@kullaniciAdi", kullaniciAdi);
+                komut.Parameters.AddWithValue("@sifre", sifre);
+                komut.Parameters.AddWithValue("@tcNo", tcNo);
+                komut.Parameters.AddWithValue("@telefonNo", telefonNo);
+                komut.Parameters.AddWithValue("@email", email);
+                komut.Parameters.AddWithValue("@adres", adres);
+                komut.Parameters.AddWithValue("@yetki", yetki);
+                komut.ExecuteNonQuery();//sogruyu execute ediyoruz
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarıyla Oluşturulmuştur.");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
